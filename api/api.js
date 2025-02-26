5432998335f98de5c96c1d86e315b138894df335
ricpaula <cadodepaulasilva159@gmail.com>
2025-02-26 20:58:56 -0300

    Update api axios

--- api/api.js
+++ api/api.js
@@ -1,10 +1,9 @@
-import axios from 'axios';
-axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+import axios from "axios";
 
 const URL = "https://projeto-fullstack-spotify-backend.vercel.app/";
 
-const responseArtists = await axios.get(`${URL}/artists`)
-const responseSongs = await axios.get(`${URL}/songs`)
+const responseArtists = await axios.get(`${URL}/artists`);
+const responseSongs = await axios.get(`${URL}/songs`);
 
-export const artistArray = responseArtists.data
-export const songsArray = responseSongs.data
+export const artistArray = responseArtists.data;
+export const songsArray = responseSongs.data;
