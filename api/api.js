9b64a3a5718c7fb108cc3b13db931e0ad04adae4
ricpaula <cadodepaulasilva159@gmail.com>
2025-02-26 19:52:56 -0300

    update axios

--- api/api.js
+++ api/api.js
@@ -2,8 +2,9 @@ import axios from 'axios';
 
 const URL = "https://projeto-fullstack-spotify-backend.vercel.app/";
 
-const responseArtists = await axios.get(`${URL}/artists`)
-const responseSongs = await axios.get(`${URL}/songs`)
+const responseArtists = await axios.get(`${URL}/artists`,{headers: {'Access-Control-Allow-Origin': '*'}})
+const responseSongs = await axios.get(`${URL}/songs`,{headers: {'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+'Access-Control-Allow-Headers': "append,delete,entries,foreach,get,has,keys,set,values,Authorization"}})
 
 export const artistArray = responseArtists.data
 export const songsArray = responseSongs.data
